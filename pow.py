c26c5bd8d9df0bd906cae3c5dc9be19b1e61c573
Canário <81337238+Canario0001@users.noreply.github.com>
2022-08-17 22:36:41 -0300

    Header maior e mais bonito

--- pow.py
+++ pow.py
@@ -9,10 +9,10 @@ def result(num, vezes):
 def main():
     print('Olá! Sou um programa para calcular a tabela de potenciação de qualquer número inteiro que você inserir.')
     num = int(input('Digite o número de sua escolha: '))
-    print('-'*20)
+    print('┅'*40)
     for i in range(-5, 21):
         result(num, i)
-    print('-'*20)
+    print('┅'*40)
 
 if __name__ == '__main__':
     main()
